edf9125672a7da7137dd49e8e88b2a6efeb81f50
Tyra Afoley Okai Cudjoe <tyra20015@gmail.com>
2025-04-21 13:48:39 +0200

    Added second println in main

--- src/main/java/org/hibera/Main.java
+++ src/main/java/org/hibera/Main.java
@@ -3,5 +3,6 @@ package org.hibera;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		System.out.println("Test Project github commit!");
 	}
 }
